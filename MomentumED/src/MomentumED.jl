e69db134d5297dd85a3020ba2ceb80e32c146fc5
Alice and Bob <zou.bo.phys@gmail.com>
2025-10-31 21:07:40 -0400

    update docstring

--- MomentumED/src/MomentumED.jl
+++ MomentumED/src/MomentumED.jl
@@ -2,6 +2,43 @@
 This module gives general methods for 2D momentum-block-diagonalized ED calculations.
 Sectors of other quantum Numbers should be handled outside this module.
 This module only sets sectors of total (crystal) momentum, also called blocks.
+
+# export list
+```julia
+# submodules
+export EDCore
+public Preparation, Methods, Analysis
+
+# main solving function
+export EDsolve
+
+# from EDCore
+export MBS64, NormalScatter, MBOperator
+export HilbertSubspace, MBS64Vector, Scatter
+public get_bits, get_body, make_dict!, delete_dict!
+public isphysical, isupper, isnormal, isnormalupper, isdiagonal
+export ED_bracket, ED_bracket_threaded
+public ColexMBS64, ColexMBS64Mask
+
+# preparation
+export EDPara, ED_momentum_subspaces
+export ED_sortedScatterList_onebody
+export ED_sortedScatterList_twobody
+
+# methods
+public ED_HamiltonianMatrix_threaded, LinearMap
+
+# analysis - reduced density matrix for entanglement spectrum
+export PES_1rdm, PES_MomtBlocks, PES_MomtBlock_rdm
+export OES_NumMomtBlocks, OES_NumMomtBlock_coef
+
+# analysis - many-body connection
+export ED_connection_step, ED_connection_gaugefixing!
+
+# environment variables
+public PRINT_RECURSIVE_MOMENTUM_DIVISION
+public PRINT_TWOBODY_SCATTER_PAIRS
+```
 """
 module MomentumED
 
@@ -11,13 +48,14 @@ using SparseArrays
 using KrylovKit
 
 # submodules
-export EDCore, Preparation, Methods, Analysis
+export EDCore
+public Preparation, Methods, Analysis
 
 # main solving function
 export EDsolve
 
 # from EDCore
-export EDCore, MBS64, NormalScatter, MBOperator
+export MBS64, NormalScatter, MBOperator
 export HilbertSubspace, MBS64Vector, Scatter
 public get_bits, get_body, make_dict!, delete_dict!
 public isphysical, isupper, isnormal, isnormalupper, isdiagonal
